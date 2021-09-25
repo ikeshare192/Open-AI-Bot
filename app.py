703c72cea1dff08711a72b04767ed83c6247451a
Isaac Shareef <64051998+ikeshare192@users.noreply.github.com>
2021-09-25 03:15:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,14 +7,14 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfkjdfjkdjfekiai'
 
 @app.route('/lojackbot', methods = ['POST'])
-def lojack():
+def lojackbot():
     incoming_msg = request.values['Body']
     chat_log = session.get('chat_log')
     answer = ask(incoming_msg, chat_log)
     session['chat_log'] = append_interaction_to_chatlog(incoming_msg, answer, chat_log)
     msg = MessagingResponse()
-    msg.message(answer)
+    msg.message('this is the response')
     return str(msg)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
