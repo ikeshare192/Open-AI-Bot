50a259d8aba3e66e70e92252f9da2bd2706ea7c2
ikeshare192 <isaacshareef@hotmail.com>
2023-01-10 13:36:37 -0700

    Jan 2023 Update

--- app.py
+++ app.py
@@ -3,19 +3,23 @@ from twilio.twiml.messaging_response import MessagingResponse
 from lojackbot import ask, append_interaction_to_chatlog
 
 app = Flask(__name__)
-
-app.config['SECRET_KEY'] = 'dfkjdfjkdjfekiai'
+app.config['SECRET_KEY'] = "jdksafjkdl;as"
 
 @app.route('/lojackbot', methods = ['POST'])
 
 def lojackbot():
     incoming_msg = request.values['Body']
-    chat_log = session.get('chat_log')
-    answer = ask(incoming_msg, chat_log)
-    session['chat_log'] = append_interaction_to_chatlog(incoming_msg, answer, chat_log)
-    msg = MessagingResponse()
-    msg.message(answer)
-    return str(msg)
+    chat_log = session.get("chat_log")
 
+    answer = ask(incoming_msg, chat_log)
+    session['chat_log'] = append_interaction_to_chatlog(incoming_msg, answer,
+                                                         chat_log)
+
+    r = MessagingResponse()
+    r.message(answer)
+    return str(r)
+
+'''
 if __name__ == '__main__':
     app.run()
+'''
